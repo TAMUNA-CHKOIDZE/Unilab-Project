526156fa0901919d933fd0a1edc77bf6dc3678d4
antiadamiani <tamar.chkoidze.1@mail.iliauni.edu.ge>
2022-06-14 12:04:16 +0400

    added responsive of contact.html;

--- my.js
+++ my.js
@@ -1,14 +1,21 @@
 let mobileMenu = document.querySelector('.menu');
 let burgerMenu = document.querySelector('.header_menu_btn');
 let closeMenu = document.querySelector('.menu_close');
-let footer = document.querySelector('.footer')
-burgerMenu.addEventListener('click', function(){
+let footer = document.querySelector('.footer');
+let backBtn = document.querySelector('.news_header_text_arrow');
+
+backBtn && backBtn.addEventListener('click', function () {
+    window.location.href = "../index.html"
+})
+
+burgerMenu && burgerMenu.addEventListener('click', function () {
     mobileMenu.classList.add('active');
     footer.classList.add('active');
 })
 
-
-closeMenu.addEventListener('click', function(){
+closeMenu && closeMenu.addEventListener('click', function () {
     mobileMenu.classList.remove('active');
     footer.classList.remove('active');
 })
+
+
